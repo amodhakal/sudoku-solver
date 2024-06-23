fa4ab35bfecfe3d18db0fb2d1a915c42a35925c2
Amodh Dhakal <amodhakal@gmail.com>
2024-06-22 21:03:09 -0400

    Create a file reader

--- solver.cpp
+++ solver.cpp
@@ -1,3 +1,20 @@
-#include <cstdlib>
+#include <iostream>
 
-int main() { return EXIT_SUCCESS; }
+int main(int argc, char **argv) {
+    using namespace std;
+
+    if (argc != 2) {
+        cerr << "usage: ./solve [input-file]" << endl;
+        exit(1);
+    }
+
+    char *filename = argv[1];
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        cerr << "Unable to open file: " << filename << endl;
+        exit(1);
+    }
+
+    fclose(file);
+    return 0;
+}
